96168500695ba7cbf25acc3c29d7a1836dc0ea87
Ghulam Muhammad <95461763+20I-0923@users.noreply.github.com>
2023-10-08 18:33:48 +0500

    Second commit, fixing import errors

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module assignment01bca
+module github.com/20I-0923/assignment01bca
 
 go 1.21.2
---
+++ main.go
@@ -0,0 +1,32 @@
+// main.go
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/20I-0923/assignment01bca"
+)
+
+func main() {
+	// Create a new blockchain
+	bc := &assignment01bca.Blockchain{}
+
+	// Add blocks to the blockchain
+	bc.Blocks = append(bc.Blocks, assignment01bca.NewBlock("Transaction 1", 123, ""))
+	bc.Blocks = append(bc.Blocks, assignment01bca.NewBlock("Transaction 2", 456, bc.Blocks[len(bc.Blocks)-1].CurrentHash))
+
+	// Display the blocks
+	assignment01bca.DisplayBlocks(bc)
+
+	// Change the transaction of the second block
+	assignment01bca.ChangeBlock(bc.Blocks[1], "New Transaction 2")
+
+	// Verify the blockchain
+	isValid := assignment01bca.VerifyChain(bc)
+	if isValid {
+		fmt.Println("Blockchain is valid.")
+	} else {
+		fmt.Println("Blockchain is not valid.")
+	}
+}
